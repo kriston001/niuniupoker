b58d88f7b688671a873d41797f345b37026972e9
ZHUTAOTAO <1026102055@qq.com>
2025-05-08 17:03:29 +0800

    11

--- packages/nextjs/utils/customChains.ts
+++ packages/nextjs/utils/customChains.ts
@@ -1,35 +1,60 @@
 import { defineChain } from "viem";
 
-// Base chain
 export const monadTestnet = defineChain({
-    id: 10143, // 链ID
-    name: "Monad Testnet",
-    // network: "custom",
-    nativeCurrency: {
-        decimals: 18,
-        name: "MON",
-        symbol: "MON",
+  id: 10143, // 链ID
+  name: "Monad Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "MON",
+    symbol: "MON",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://testnet-rpc.monad.xyz/"],
     },
-    rpcUrls: {
-        default: {
-        http: ["https://testnet-rpc.monad.xyz/"],
-        },
-        public: {
-        http: ["https://testnet-rpc.monad.xyz/"],
-        },
+    public: {
+      http: ["https://testnet-rpc.monad.xyz/"],
     },
-    blockExplorers: {
-        default: {
-        name: "Monad Testnet",
-        url: "https://testnet.monadexplorer.com/",
-        },
+  },
+  blockExplorers: {
+    default: {
+      name: "Monad Testnet",
+      url: "https://testnet.monadexplorer.com/",
     },
-    contracts: {
-        multicall3: {
-        address: "0xcA11bde05977b3631167028862bE2a173976CA11",
-        },
-        BBGameMain: {
-        address: "0x57beFAAeA57Df35E54fFBc4076d62672c66B3ca9", // 替换为您在 Monad Testnet 上部署的 BBGameMain 合约地址
-        },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
     },
+    BBGameMain: {
+      address: "0x57beFAAeA57Df35E54fFBc4076d62672c66B3ca9", // 替换为您在网络上部署的 BBGameMain 合约地址
+    },
+  },
+});
+
+export const riseTestnet = defineChain({
+  id: 11155931, // Chain ID
+  name: "RISE Testnet",
+  network: "rise-testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: { http: ["https://testnet.riselabs.xyz"] },
+    default: { http: ["https://testnet.riselabs.xyz"] },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://explorer.testnet.riselabs.xyz" },
+  },
+  testnet: true,
+  contracts: {
+    multicall3: {
+      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+    },
+    BBGameMain: {
+      address: "0x57beFAAeA57Df35E54fFBc4076d62672c66B3ca9", // 替换为您在网络上部署的 BBGameMain 合约地址
+    },
+  },
 });
